74345f9a6a5cf7c7ad7f5467d03d5d71be6e0c20
Coline Forde <coline.forde@gmail.com>
2016-03-03 18:58:43 -0800

    refactor code to reroute homepage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
   validates :full_name, :email, :username, :password_hash, presence: true
+
+   def password 
+  	@password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(new_password)
+  	@password = BCrypt::Password.create(new_password)
+  	self.password_hash = @password
+  end
+
+  def authenticate(password)
+  	self.password == password
+  end
 end
