42252597a96f50a00ff0bf2c9ec127113435db90
Sharon <email": null>
2016-03-03 19:09:04 -0800

    Seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-30.times { User.create(full_name: Faker::Name.name, email: Faker::Internet.email, username: Faker::Internet.user_name, password: Faker::Name.password) }
+30.times { User.create(full_name: Faker::Name.name, email: Faker::Internet.email, username: Faker::Internet.user_name, password: Faker::Internet.password) }
 
